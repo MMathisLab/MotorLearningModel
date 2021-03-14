501cac3b784d1013f2fac23d30545572f38d8fd6
Mackenzie Mathis <mathis@rowland.harvard.edu>
2021-03-14 17:38:28 +0100

    remove return 0 for now...

--- KalmanFilter/KalmanFilter-learning.py
+++ KalmanFilter/KalmanFilter-learning.py
@@ -7,7 +7,7 @@ def SimulatingModel_long(sigma_m,sigma_s,sigma_p,a,distanceduringperturbation):
 	''' Simulating Kalman-filter-based learning-model for a set of model parameters. This code is inefficient and stores many
 	intermediary variables. For speed use the equivalent "SimulatingModel_fast" function. '''
 
-	# Trialstructure from paper:   
+	# Trialstructure from paper:
 	baseline=75             # number of trials
 	perturbation=100        # number of trials
 	washout=75              # number of trials
@@ -18,13 +18,13 @@ def SimulatingModel_long(sigma_m,sigma_s,sigma_p,a,distanceduringperturbation):
 	Disturbance[baseline:baseline+perturbation]=distanceduringperturbation
 
 	# Model definitions
-	F=np.array([[a,0],[1,0]])                           # state transition matrix 
+	F=np.array([[a,0],[1,0]])                           # state transition matrix
 	Q=np.array([[sigma_p**2,0],[0,sigma_m**2]])         # state noise
 
 	K=np.zeros((2,experimentnumtrials))         # Kalman Gain
 	b=np.array([0, 1])                          # control matrix
 	H=np.array([0, 1])                          # measurment matrix
-	P=np.zeros((2,2))                           # covariance matrix - we assume that mouse is initially certain about state 
+	P=np.zeros((2,2))                           # covariance matrix - we assume that mouse is initially certain about state
 
 	# Model variables
 	paw_position=np.zeros(experimentnumtrials)       # actual paw position
@@ -33,12 +33,12 @@ def SimulatingModel_long(sigma_m,sigma_s,sigma_p,a,distanceduringperturbation):
 	y=np.zeros(experimentnumtrials)                  # sensed paw position
 	eta_motor=sigma_m*np.random.randn(experimentnumtrials)            #noise motor
 	eta_sensory=sigma_s*np.random.randn(experimentnumtrials)          #noise sensory
-	eta_disturbance=sigma_p*np.random.randn(experimentnumtrials)      #internal model of disturbance noise  
+	eta_disturbance=sigma_p*np.random.randn(experimentnumtrials)      #internal model of disturbance noise
 
 	for t in range(experimentnumtrials-1):
 		paw_position[t]=u[t]+eta_motor[t]+Disturbance[t]+eta_disturbance[t]		# paw position depends on motor command and disturbance!
 		y[t]=paw_position[t]+eta_sensory[t]		# Sensed paw position depends on disturbed paw position and sensory noise
-		if t>75: 				
+		if t>75:
 			# Start learning with Kalman-filter during perturbation and washout blocks.
 			# Forward model - (a-priori) prediction step
 			x_apriori=np.dot(F,xhat[:,t-1])+b*u[t] #order: (perturbation , hand pos.) A priori estimate (without) sensory feedback
@@ -48,7 +48,7 @@ def SimulatingModel_long(sigma_m,sigma_s,sigma_p,a,distanceduringperturbation):
 
 			# A-posteriori estimate and uncertainty
 			xhat[:,t]=x_apriori+K[:,t]*(y[t]-np.dot(H,x_apriori))
-			P=np.dot(np.eye(2)-np.outer(K[:,t],H),P) 		     	  # Updating P (i.e. P k | k = ... 
+			P=np.dot(np.eye(2)-np.outer(K[:,t],H),P) 		     	  # Updating P (i.e. P k | k = ...
 
 			# Extracting motor command for next trial (= ideal estimate)
 			u[t+1]=-xhat[0,t]
@@ -59,9 +59,9 @@ def SimulatingModel_long(sigma_m,sigma_s,sigma_p,a,distanceduringperturbation):
 def SimulatingModel_fast(sigma_m,sigma_s,sigma_p,a,distanceduringperturbation):
     ''' Simulating Kalman-filter-based learning-model for a set of model parameters. This code is inefficient and stores many
 	intermediary variables. For speed use the equivalent "SimulatingModel_fast" function. '''
-    
+
 	#to be done
-	return 0
+	#return 0
 
 
 
@@ -70,7 +70,7 @@ if __name__ in '__main__':
 
 	## Loading experimental and fit data
 	with open("Fig2D_data.pickle","rb") as f:
-		data,data_errors,sigma_m,sigma_s,sigma_p,a,distanceduringperturbation=pickle.load(f) 
+		data,data_errors,sigma_m,sigma_s,sigma_p,a,distanceduringperturbation=pickle.load(f)
 
 	numrealizations=10      #number of realization of (stochastic) model
 
@@ -80,10 +80,10 @@ if __name__ in '__main__':
 	upperbound=.075
 	lwidth=1
 
-	plt.figure(figsize=(6, 5))    
-	ax = plt.subplot(111)    
-	ax.spines["top"].set_visible(False)    
-	ax.spines["right"].set_visible(False)    
+	plt.figure(figsize=(6, 5))
+	ax = plt.subplot(111)
+	ax.spines["top"].set_visible(False)
+	ax.spines["right"].set_visible(False)
 
 	# Run model for fitted parameters - gives one random iteration
 	for jj in range(numrealizations):
@@ -106,6 +106,3 @@ if __name__ in '__main__':
 	plt.ylim(lowerbound,upperbound)
 
 	plt.savefig("Figure2D_realization.pdf")
-
-
-	 
